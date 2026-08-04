2bc26973938fd7bef33e57f614fbf2a3e6c80af5
Omar Morshed <omarmimorshed79@gmail.com>
2026-08-04 22:11:06 +0300

    feature: added the BaseEntity Class in the Domains/Entities

---
+++ src/TalebElm.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TalebElm.Domain.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
